61e7b4d41c308e20fc3abb934132264122c83c33
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2023-11-09 13:03:33 +0100

    cc

--- ejercicio2/builder/lanzador.py
+++ ejercicio2/builder/lanzador.py
@@ -1,12 +1,9 @@
 from pizzeria import Pizzeria
 from director import Director
 from menu import Menu
+from jamonyqueso import Constructorjamonyqueso
 
 if __name__ == '__main__':
-    
-#ejecutar el programa
 
     director = Director()
-    builder = Pizzeria()
-    director.builder = builder
     menu = Menu()
